38b8cab4a56c417b3ddf17bd7ee24e815074a118
bhavani <durgabhavani.mupparthi@gmail.com>
2024-09-17 20:11:41 +0000

    commit message

---
+++ src/main/java/com/example/goodreads/repository/BookRepository.java
@@ -0,0 +1,21 @@
+package com.example.goodreads.repository;
+
+import java.util.*;
+
+import com.example.goodreads.model.*;
+
+public interface BookRepository {
+    ArrayList<Book> getBooks();
+
+    Book getBookById(int bookId);
+
+    Book addBook(Book book);
+
+    Book updateBook(int bookId, Book book);
+
+    void deleteBook(int bookId);
+
+    Publisher getBookPublisher(int bookId);
+
+    List<Author> getBookAuthors(int bookId);
+}
